09f243c01ba062006d4b0f5404d07e86e84baff9
Yiorgis Gozadinos <ggozad@jarn.com>
2011-03-30 16:52:30 +0200

    Added Tema portlet

---
+++ jarn/kommuner/browser/los_portlet.py
@@ -0,0 +1,51 @@
+from Acquisition import aq_inner
+from plone.app.portlets.portlets import base
+from plone.memoize.compress import xhtml_compress
+from plone.memoize.instance import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements
+
+from jarn.kommuner import kommunerMessageFactory as _
+
+
+class ILOSPortlet(IPortletDataProvider):
+    """A portlet to render open calls for proposal
+    """
+
+
+class Assignment(base.Assignment):
+    implements(ILOSPortlet)
+
+    title = _(u'LOS portlet')
+
+
+class Renderer(base.Renderer):
+
+    _template = ViewPageTemplateFile('los_portlet.pt')
+
+    def render(self):
+        return xhtml_compress(self._template())
+
+    @property
+    def available(self):
+        return len(self._data())
+
+    def LOS(self):
+        return self._data()
+
+    @memoize
+    def _data(self):
+        context = aq_inner(self.context)
+        portal = getToolByName(context, 'portal_url').getPortalObject()
+        if 'tema' not in portal:
+            return []
+        tema = portal['tema']
+        return tema.getFolderContents()
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
