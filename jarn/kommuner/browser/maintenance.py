d9ee4194084b7f48d576be0e9dac470c01e10c7f
Yiorgis Gozadinos <ggozad@jarn.com>
2011-04-26 15:08:49 +0200

    Make catalog update periodic

--- jarn/kommuner/browser/maintenance.py
+++ jarn/kommuner/browser/maintenance.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 import logging
+
+from plone.registry.interfaces import IRegistry
 from Products.Five.browser import BrowserView
+from zope.component import getUtility
 
 from jarn.kommuner import sd_content
 
@@ -15,6 +19,12 @@ class ImportActiveServiceDescriptionsView(BrowserView):
 class UpdateActiveServiceDescriptionsView(BrowserView):
 
     def __call__(self):
-        logger.info("clockserver ping")
-        return
-        sd_content.updateActiveServiceDescriptions(self.context)
+        registry = getUtility(IRegistry)
+        last_update = registry['jarn.kommuner.lastUpdate']
+        now = datetime.now()
+        if last_update is None:
+            logger.error('Error checking for updated national catalog updates, please import them.')
+            return
+        if (now-last_update).days > 1:
+            logger.info('Checking for updated national catalog updates')
+            sd_content.updateActiveServiceDescriptions(self.context)
