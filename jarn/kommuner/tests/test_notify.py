fe506e2ac6c9012530ff370703309b2ed2dcaea0
Stefan H. Holek <stefan@jarn.com>
2011-04-29 12:47:04 +0200

    Record last notification time.

---
+++ jarn/kommuner/tests/test_notify.py
@@ -0,0 +1,15 @@
+import unittest2 as unittest
+
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from jarn.kommuner import sd_content
+from jarn.kommuner.testing import KOMMUNER_INTEGRATION_TESTING
+
+
+class NotificationTest(unittest.TestCase):
+
+    layer = KOMMUNER_INTEGRATION_TESTING
+
+    def test_registry_keys(self):
+        registry = getUtility(IRegistry)
+        self.assertTrue('jarn.kommuner.lastNotified' in registry)
