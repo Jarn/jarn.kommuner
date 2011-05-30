ea3e2c9f461fbb09362e0e5fb157d7baef154f59
Yiorgis Gozadinos <ggozad@jarn.com>
2011-05-30 14:57:28 +0200

    Added contact person to service description

--- jarn/kommuner/content/service_description.py
+++ jarn/kommuner/content/service_description.py
@@ -5,12 +5,21 @@ from Products.ATContentTypes.content import schemata
 from Products.ATContentTypes.interface import IATContentType
 from Products.ATContentTypes.content.base import ATCTContent
 from Products.ATContentTypes.content.base import ATContentTypeSchema
+from Products.CMFCore.utils import getToolByName
 from zope.interface import implements
 
 from jarn.kommuner.config import PROJECTNAME
 from jarn.kommuner.interfaces import IServiceDescription
 from jarn.kommuner.vocabularies import losCategoriesRefs
 from jarn.kommuner import kommunerMessageFactory as _
+
+
+def listPersons(context):
+    ct = getToolByName(context, 'portal_catalog')
+    brains = ct.searchResults(portal_type='Person')
+    results = [(item.UID, item.Title, ) for item in brains]
+    results.insert(0, ('', '--None--'))
+    return atapi.DisplayList(results)
 
 
 ServiceDescriptionSchema = ATContentTypeSchema.copy() + atapi.Schema((
@@ -61,6 +70,19 @@ ServiceDescriptionSchema = ATContentTypeSchema.copy() + atapi.Schema((
             label = _(u'Service Id')),
     ),
 
+    atapi.ReferenceField(
+        'contacts',
+        multiValued=True,
+        allowed_types=('Person', ),
+        relationship='contacts_cfp_person',
+        widget=atapi.ReferenceWidget(
+            label=_(u"Contact persons"),
+        ),
+        vocabulary='listContacts',
+        languageIndependent=True,
+        required=False,
+    ),
+
 ))
 
 ServiceDescriptionSchema['nationalText'].widget.visible = \
@@ -84,5 +106,8 @@ class ServiceDescription(ATCTContent):
 
     def losCategoriesVocab(self):
         return losCategoriesRefs(self)
+
+    def listContacts(self):
+        return listPersons(self)
 
 atapi.registerType(ServiceDescription, PROJECTNAME)
