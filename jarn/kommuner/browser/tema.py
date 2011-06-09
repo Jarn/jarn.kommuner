443d4cdd690a1bec221dc99272d8c039540bfd09
Yiorgis Gozadinos <ggozad@jarn.com>
2011-06-09 10:46:11 +0200

    Added tema view

---
+++ jarn/kommuner/browser/tema.py
@@ -0,0 +1,21 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+
+
+class TemaView(BrowserView):
+
+    def los(self):
+        tema_path = '/'.join(self.context.getPhysicalPath())
+        ct = getToolByName(self.context, 'portal_catalog')
+        root_categories = ct.searchResults(
+            portal_type='LOSCategory',
+            path={'query': tema_path, 'depth':1},
+            sort_on='sortable_title')
+        results = []
+        for brain in root_categories:
+            child_categories = ct.searchResults(
+                portal_type='LOSCategory',
+                path={'query': brain.getPath(), 'depth':1},
+                sort_on='sortable_title')
+            results.append([brain, child_categories])
+        return results
