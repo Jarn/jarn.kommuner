fe865b3378f474e459eefb3616fa465bdc5cf0b0
Denys Mishunov <denys.mishunov@gmail.com>
2011-07-29 10:23:38 +0200

    python classes for the frontpage view and manage

---
+++ jarn/kommuner/browser/manage_frontpage.py
@@ -0,0 +1,8 @@
+from plone.app.portlets.browser.manage import ManageContextualPortlets
+
+
+class UtsiraManageContextualPortlets(ManageContextualPortlets):
+
+    def __init__(self, context, request):
+        # Note that we omit our own super class
+        super(ManageContextualPortlets, self).__init__(context, request)
