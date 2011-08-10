9a7df7aa8df133d000b53712dfb30faa14262411
Stefan H. Holek <stefan@jarn.com>
2011-08-10 14:46:06 +0200

    Allow anonymous to view the front-page.

--- jarn/kommuner/browser/frontpage.py
+++ jarn/kommuner/browser/frontpage.py
@@ -1,5 +1,9 @@
 from zope.publisher.browser import BrowserView
+from AccessControl import getSecurityManager
 
 
 class FrontPageView(BrowserView):
-    pass
+
+    def canManagePortlets(self):
+        sm = getSecurityManager()
+        return sm.checkPermission("Portlets: Manage portlets", self.context)
