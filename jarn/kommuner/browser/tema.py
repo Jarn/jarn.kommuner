d5f50971f97df6a2108c51710ce94860690af405
Stefan H. Holek <stefan@jarn.com>
2011-09-22 11:16:03 +0200

    Add tema_sd_view.

--- jarn/kommuner/browser/tema.py
+++ jarn/kommuner/browser/tema.py
@@ -19,3 +19,15 @@ class TemaView(BrowserView):
                 sort_on='sortable_title')
             results.append([brain, child_categories])
         return results
+
+
+class TemaServiceDescriptionView(BrowserView):
+
+    def services(self):
+        tema_path = '/'.join(self.context.getPhysicalPath())
+        ct = getToolByName(self.context, 'portal_catalog')
+        results = ct.searchResults(
+            portal_type='ServiceDescription',
+            path={'query': tema_path, 'depth':1},
+            sort_on='sortable_title')
+        return results
